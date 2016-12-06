1be33dac187f17e80ed7b33f2752118f7006b85a
root <root>
2016-12-06 17:54:33 +0200

    Added realisation to tomcat server, model and dao

--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -3,5 +3,23 @@ package config;
 /**
  * Created by employee on 12/6/16.
  */
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+@ComponentScan(basePackages = {"core", "config"})
+@Import({HibernateConfig.class})
 public class AppConfig {
+
+    @Bean
+    public PropertyPlaceholderConfigurer propertyPlaceholderConfigurer() {
+        PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
+        ppc.setLocation(new ClassPathResource("database.properties"));
+        ppc.setIgnoreUnresolvablePlaceholders(true);
+        return ppc;
+    }
 }
--- src/main/java/server/Server.java
+++
@@ -1,8 +0,0 @@
-package server;
-
-/**
- * Created by employee on 12/6/16.
- */
-public class Server {
-
-}
